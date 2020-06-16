68c3de76d4a801ee30183091a61f5cc11e470098
Vyacheslav <slavasan05@gmail.com>
2020-06-16 23:15:29 +0300

    hw-06

---
+++ hw-06/js/task-3.js
@@ -0,0 +1,8 @@
+import { users } from "./users.js";
+
+const getUsersWithGender = (users, gender) => {
+    return users.filter(user => user.gender === gender)
+    .map(obgectOnGender => obgectOnGender.name);
+};
+
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
