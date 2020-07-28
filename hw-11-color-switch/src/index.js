864650be18f62df04ae1575da51b15a0cf7d0c20
Vyacheslav <slavasan05@gmail.com>
2020-07-28 22:17:58 +0300

    reworked on class

--- hw-11-color-switch/src/index.js
+++ hw-11-color-switch/src/index.js
@@ -16,20 +16,39 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-const GenerateRandomColor = {
-    intervalId: null,
-    start() {
-         this.intervalId = setInterval(() => {
-        document.body.style = `background-color: ${colors[randomIntegerFromInterval(0, colors.length - 1)]};`;
-        }, 1000);
-        ref.buttonStart.disabled = true;
-    },
-    stop() {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
-        ref.buttonStart.disabled = false;
+class GenerateRandomColor {
+    constructor () {
+    this.intervalId = null;  
     }
-} 
+start() {
+    this.intervalId = setInterval(() => {
+        document.body.style = `background-color: ${colors[randomIntegerFromInterval(0, colors.length - 1)]};`;
+    }, 1000);
+    ref.buttonStart.disabled = true;
+}
+stop() {
+    console.log(this)
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+    ref.buttonStart.disabled = false;
+}
+}
+const randomColor = new GenerateRandomColor();
+// randomColor.start
+// const GenerateRandomColor = {
+//     intervalId: null,
+//     start() {
+//          this.intervalId = setInterval(() => {
+//         document.body.style = `background-color: ${colors[randomIntegerFromInterval(0, colors.length - 1)]};`;
+//         }, 1000);
+//         ref.buttonStart.disabled = true;
+//     },
+//     stop() {
+//         clearInterval(this.intervalId);
+//         this.intervalId = null;
+//         ref.buttonStart.disabled = false;
+//     }
+// } 
 
-ref.buttonStart.addEventListener('click', GenerateRandomColor.start.bind(GenerateRandomColor));
-ref.buttonStop.addEventListener('click', GenerateRandomColor.stop.bind(GenerateRandomColor));
+ref.buttonStart.addEventListener('click', randomColor.start.bind(randomColor));
+ref.buttonStop.addEventListener('click', randomColor.stop.bind(randomColor));
