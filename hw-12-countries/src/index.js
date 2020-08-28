3de4cc42c24adeba5d583021346c4f4cfed39a71
Vyacheslav <slavasan05@gmail.com>
2020-08-29 00:46:01 +0300

    add feature

--- hw-12-countries/src/index.js
+++ hw-12-countries/src/index.js
@@ -1,14 +1,30 @@
 import './styles.css';
 import fetchCountries from './js/fetchCountries';
 import refs from './js/refs';
+import markupCountry from './js/markup';
+import templatesOneCounry from './templates/templatesOneCounry.hbs';
+import templateCountryList from './templates/templateCountryList.hbs';
+import templateWarn from './templates/templateWarn.hbs';
 
 var debounce = require('lodash.debounce');
+
+function sampleOfCountries(countries) {
+  if (countries.length === 1) {
+    return markupCountry(countries, templatesOneCounry);
+  } else if (countries.length > 1 && countries.length <= 10) {
+    return markupCountry(countries, templateCountryList);
+  } else if (countries.length > 10) {
+    return markupCountry(countries, templateWarn);
+  }
+}
 
 refs.searchForm.addEventListener(
   'input',
   debounce(e => {
     refs.counrtyContainer.innerHTML = '';
-    fetchCountries(e.target.value);
+    fetchCountries(e.target.value).then(country => {
+      sampleOfCountries(country);
+    });
     refs.spiner.classList.remove('loader');
   }, 1000),
 );
@@ -17,3 +33,21 @@ refs.searchForm.addEventListener(
     refs.spiner.classList.add('loader');
   }
 );
+refs.counrtyContainer.addEventListener('click', debounce(e => {
+  if (e.target.className === 'country-item') {
+    fetchCountries(e.target.innerHTML).then(country => {
+      country.forEach((item) => {
+        if (e.target.innerHTML === item.name) {
+          sampleOfCountries([item]);
+          refs.spiner.classList.remove('loader');
+        }
+      })
+    })
+  }
+}, 1000))
+refs.counrtyContainer.addEventListener('click', (e) => {
+  if (e.target.className === 'country-item') {
+    refs.counrtyContainer.innerHTML = '';
+    refs.spiner.classList.add('loader');
+  }
+})
--- hw-12-countries/src/js/fetchCountries.js
+++ hw-12-countries/src/js/fetchCountries.js
@@ -1,26 +1,8 @@
-import markupCountry from './markup';
-import templatesOneCounry from '../templates/templatesOneCounry.hbs';
-import templateCountryList from '../templates/templateCountryList.hbs';
-import templateWarn from '../templates/templateWarn.hbs';
-
-function sampleOfCountries(countries) {
-  if (countries.length === 1) {
-      return markupCountry(countries, templatesOneCounry);
-  } else if (countries.length > 1 && countries.length <= 10) {
-      return markupCountry(countries, templateCountryList);
-  } else if (countries.length > 10) {
-    return markupCountry(countries, templateWarn);
-  }
-}
-
 function fetchCountries(queryCounrty) {
     const baseUrl = 'https://restcountries.eu/rest/v2/';
 
   return fetch(baseUrl + `name/${queryCounrty}`)
     .then(res => res.json())
-    .then(country => {
-        sampleOfCountries(country);
-    })
     .catch(err => err);
 }
 
