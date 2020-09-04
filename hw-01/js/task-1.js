df2d975f06862e50a5a018418eba36cedd89c6df
Vyacheslav <slavasan05@gmail.com>
2020-09-05 00:47:27 +0300

    test 2

--- hw-01/js/task-1.js
+++ hw-01/js/task-1.js
@@ -4,11 +4,9 @@ let price = 1000;
 console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
 
 price = 2000;
-<<<<<<< HEAD
 console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
-=======
 console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
 
 
-console.log('hello world!!!')
->>>>>>> b6ef666b7619c280cd57d7af33ab22072e775fd5
+console.log('hello world!!!');
+ const a = 10;
